bd940e67d51a2e361581ea684fb9470bf740fab4
shubham <spl.sp999@gmail.com>
2022-12-20 22:05:29 +0530

    final commit

--- 03-day.js
+++ 03-day.js
@@ -5,36 +5,35 @@
 const number=[1,2,3,4,45,8,9,0,2];
 
 
-/*console.log("this is foreach() method");
+console.log("this is foreach() method");
 number.forEach(function(el)
 {
-console.log(el);
+console.log(el*2);
 })
 
- //  using arrow function
+ // using arrow function
 number.forEach(  (el)=>{
-    console.log(el);
+   console.log(el);
 
 });
 
 //map methods
-
 console.log("this is map() method");
-/*const double=number.map(function(numbers){
+const double=number.map(function(numbers){
 
     console.log(numbers*2)
-})*/
+})
 
 
-//print even numbers
-//console.log("even numbers")
-//const even = number.map(function (el){
-    //if(el%2==0){
-    //    console.log(el);
-  //  }
-//})
+///print even numbers
+console.log("even numbers")
+const even = number.map(function (el){
+  if(el%2==0){
+        console.log(el);
+    }
+})
 
-/*const few=number.map((el)=>{
+const few=number.map((el)=>{
   if(el%2==0){
     console.log(el);
 }
@@ -60,3 +59,21 @@ const name=(el)=>{
 
 
 name('hello')
+
+//settimeout
+setTimeout(()=>{
+    console.log('this is time out function')
+},2000);
+
+//setinterval
+let Id=setInterval(() => {
+    console.log("hi")
+    
+}, 1000);
+clearInterval(Id)
+//Filter method
+const nums=[1.2,3,4,5,4,66,7,7,9];
+
+const newNum=nums.filter(n=> n%2===0)
+
+console.log(newNum)
