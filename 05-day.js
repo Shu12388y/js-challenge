c4f54b63e8f8eab26d8e072a4a9f87de262e4103
shubham <spl.sp999@gmail.com>
2022-12-22 20:09:28 +0530

    05-day

---
+++ 05-day.js
@@ -0,0 +1,31 @@
+// destruction and DOM manipulation
+
+const name = document.getElementById('name')
+
+
+
+let new_text = prompt("enter yor name")
+
+
+// const loop = (lopps => {
+//     if (lopps === "null") {
+//         name.innerHTML = new_text
+//     }
+//     else {
+//         alert("It is a alert")
+//     }
+// })
+
+
+if (new_text != "null") {
+    name.innerHTML = new_text
+}
+
+
+
+else { 
+
+}
+
+
+
