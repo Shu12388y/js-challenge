388a2d15cf3250c9257d0752cb849793eaeefe11
shubham <spl.sp999@gmail.com>
2022-12-23 19:58:33 +0530

    06-day

---
+++ 06-day.js
@@ -0,0 +1,40 @@
+// let element = document.querySelector('span')
+// element.innerHTML="Disgusting"
+
+
+// const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+//  const firstNames=fullNames.map((el)=>{
+//     return el.first
+//  })
+// console.log(firstNames)
+
+
+//greet function
+// const greet=(greetings)=>{
+//     console.log(greetings)
+// }
+
+// greet("shubham")
+
+// const span=document.querySelector("#span")
+// span.innerHTML = "<p>This is paragraph</p>"
+
+
+//destructing object
+const User={
+    firstName:"shubham",
+    lastName:"Paul",
+    email:"spl.sp99@gmail.com",
+    born:2001,
+    password:"12unfrnn",
+    bio:"Web Devloper"
+}
+const{firstName, lastName} =User
+// console.log(firstName)
+
+const header=document.getElementById("span")
+header.innerHTML="hello"
+header.getAttribute(span)
+
+// header.setAttribute(span,"https://www.google.com")
